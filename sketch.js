5b6af5b121648a8e7e0571ac2164fbf8a598eba2
leeyeji <miracle458393@gmail.com>
2014-10-16 16:23:43 +0900

    add 1
    1

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
-funcion setup()
-{
-	createCanvas(800, 800);
-	background(0);
+function setup()
+ {
+  createCanvas(800, 600);
+  background(0);
 }
 
-funcion draw()
+  function draw()
 {
-	background(0);
-	fill(255);
-	ellipse(mouseX, mouseY, 100, 100);
+  background(0);
+  fill(255);
+   ellipse(mouseX, mouseY, 100, 100);
+
+   drawDonut1(168, 239, 76, 100, 100);
+   drawDonut1(250, 110, 8, 300, 100);
+   drawDonut1(254, 243, 65, 400, 200);
+   drawDonut1(34, 247, 129, 500, 300);
 }
+
+
+function drawDonut1(R, G, B, X, Y)
+{
+  //doughnut #1
+   fill(R, G, B);
+   ellipse(X, Y, 200, 200);
+   fill(0);
+   ellipse(X, Y, 120, 120);
+}
+
+
+
+
